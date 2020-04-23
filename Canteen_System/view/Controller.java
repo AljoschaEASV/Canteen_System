36f7fea5769359c4fdba09c364db992db81241ee
Jonas <jonas.black@hush.ai>
2020-04-23 08:15:43 +0200

    redone tableview, dynamical filling, now possible to work on just one tableview entirely

--- Canteen_System/view/Controller.java
+++ Canteen_System/view/Controller.java
@@ -9,10 +9,13 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.util.Callback;
 
@@ -30,30 +33,28 @@ public class Controller {
     @FXML
     private JFXListView<?> orderList;
     @FXML
-    private TableView tableview_warmlunch, tableview_fruit, tableview_drinks, tableview_orderBasket;
+    private TableView tableview_items;
 
-    private ObservableList<ObservableList> warmlunchList = FXCollections.observableArrayList();
-    private ObservableList<ObservableList> fruitList = FXCollections.observableArrayList();
-    private ObservableList<ObservableList> drinksList = FXCollections.observableArrayList();
-    private ObservableList<ObservableList> basketList = FXCollections.observableArrayList();
+    private ObservableList<ObservableList> itemsList = FXCollections.observableArrayList();
 
     @FXML
     private void initialize() throws SQLException {
 
-    fillWarmLunchList();
-    fillFruitList();
-    fillDrinksList();
+    fillList("WarmLunch");
 
     }
 
-    public void handleButtonAction(javafx.event.ActionEvent event) {
+    public void handleButtonAction(javafx.event.ActionEvent event) throws SQLException {
         {
             if (event.getSource() == btnWarmLunch) {
-                tableview_warmlunch.toFront();
+                tableview_items.toFront();
+                fillList("WarmLunch");
             } else if (event.getSource() == btnDrinks) {
-                tableview_drinks.toFront();
+                tableview_items.toFront();
+                fillList("Drink");
             } else if (event.getSource() == btnFruits) {
-                tableview_fruit.toFront();
+                tableview_items.toFront();
+                fillList("Fruit");
             } else if (event.getSource() == btnCheckOut) {
                 pnl_checkout.toFront();
             }
@@ -73,8 +74,26 @@ public class Controller {
 
     }
 
-    public void addItemToLBasket() {
-    }
+    /*public void addItemToBasket() {
+        try {
+            tbData.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    if(event.getButton().equals(MouseButton.PRIMARY)) {
+                        if (event.getClickCount() == 2) {
+                            MediaFile selection = tbData.getSelectionModel().getSelectedItem();
+
+                            mainController.runSingleChoice(selection);
+                        }
+
+                    }
+                }
+            });
+        }catch (NullPointerException e)
+        {
+            System.out.println("Error");
+        }
+    }*/
 
 
     public void deleteItemFromBasket() {
@@ -105,12 +124,13 @@ public class Controller {
     }
 
 
-    private void fillWarmLunchList() throws SQLException {
-        warmlunchList.clear();
-        tableview_warmlunch.refresh();
+    private void fillList(String itemCategory) throws SQLException {
+        itemsList.clear();
+        tableview_items.getColumns().clear();
+        tableview_items.refresh();
 
         //filling warmlunch tableview
-        DBwrapper.select("select ProductName, Price, GrammOrMl, ProductDescription from ItemsWithCategory where CategoryName='WarmLunch'");
+        DBwrapper.select("select ProductName, Price, GrammOrMl, ProductDescription, Amount from ItemsWithCategoryAndStock where CategoryName='" + itemCategory + "'");
 
         for(int i=0 ; i<DBwrapper.getResultSet().getMetaData().getColumnCount(); i++){
             //We are using non property style for making dynamic table
@@ -118,7 +138,7 @@ public class Controller {
             TableColumn col = new TableColumn(DBwrapper.getResultSet().getMetaData().getColumnName(i+1));
             col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param -> new SimpleStringProperty(param.getValue().get(j).toString()));
 
-            tableview_warmlunch.getColumns().addAll(col);
+            tableview_items.getColumns().addAll(col);
             System.out.println("Column ["+i+"] ");
         }
 
@@ -130,69 +150,9 @@ public class Controller {
                 row.add(DBwrapper.getResultSet().getString(i));
             }
             System.out.println("Row [1] added "+row );
-            warmlunchList.add(row);
+            itemsList.add(row);
         }
-        tableview_warmlunch.setItems(warmlunchList);
-    }
-
-    private void fillFruitList() throws SQLException {
-        fruitList.clear();
-        tableview_fruit.refresh();
-
-        //filling fruitList tableview
-        DBwrapper.select("select ProductName, Price, GrammOrMl, ProductDescription from ItemsWithCategory where CategoryName='Fruit'");
-
-        for(int i=0 ; i<DBwrapper.getResultSet().getMetaData().getColumnCount(); i++){
-            //We are using non property style for making dynamic table
-            final int j = i;
-            TableColumn col = new TableColumn(DBwrapper.getResultSet().getMetaData().getColumnName(i+1));
-            col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param -> new SimpleStringProperty(param.getValue().get(j).toString()));
-
-            tableview_fruit.getColumns().addAll(col);
-            System.out.println("Column ["+i+"] ");
-        }
-
-        while(DBwrapper.getResultSet().next()){
-            //Iterate Row
-            ObservableList<String> row = FXCollections.observableArrayList();
-            for(int i=1 ; i<=DBwrapper.getResultSet().getMetaData().getColumnCount(); i++){
-                //Iterate Column
-                row.add(DBwrapper.getResultSet().getString(i));
-            }
-            System.out.println("Row [1] added "+row );
-            fruitList.add(row);
-        }
-        tableview_fruit.setItems(fruitList);
-    }
-
-    private void fillDrinksList() throws SQLException {
-        drinksList.clear();
-        tableview_drinks.refresh();
-
-        //filling fruitList tableview
-        DBwrapper.select("select ProductName, Price, GrammOrMl, ProductDescription from ItemsWithCategory where CategoryName='Drink'");
-
-        for(int i=0 ; i<DBwrapper.getResultSet().getMetaData().getColumnCount(); i++){
-            //We are using non property style for making dynamic table
-            final int j = i;
-            TableColumn col = new TableColumn(DBwrapper.getResultSet().getMetaData().getColumnName(i+1));
-            col.setCellValueFactory((Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>) param -> new SimpleStringProperty(param.getValue().get(j).toString()));
-
-            tableview_drinks.getColumns().addAll(col);
-            System.out.println("Column ["+i+"] ");
-        }
-
-        while(DBwrapper.getResultSet().next()){
-            //Iterate Row
-            ObservableList<String> row = FXCollections.observableArrayList();
-            for(int i=1 ; i<=DBwrapper.getResultSet().getMetaData().getColumnCount(); i++){
-                //Iterate Column
-                row.add(DBwrapper.getResultSet().getString(i));
-            }
-            System.out.println("Row [1] added "+row );
-            drinksList.add(row);
-        }
-        tableview_drinks.setItems(drinksList);
+        tableview_items.setItems(itemsList);
     }
 
 }
