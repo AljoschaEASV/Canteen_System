0f533a5d4b2046da03f12c8d2094b1f988cb667d
Jonas <jonas.black@hush.ai>
2020-04-23 02:34:43 +0200

    deleted connection variable out of main class

--- Canteen_System/domain/Main.java
+++ Canteen_System/domain/Main.java
@@ -6,13 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import Application.DatabaseConnector;
-
 public class Main extends Application {
 
     public static void main(String[] args) {
-        DatabaseConnector n = new DatabaseConnector();
-        n.createConnection();
         launch(args);
     }
 
